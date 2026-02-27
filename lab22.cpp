5c1ebcacc569c5606cc292949a5d0a8e3eac6df2
pxax5 <jiraphat9298@gmail.com>
2026-02-27 22:28:25 +0700

    lab22 is complete

--- lab22.cpp
+++ lab22.cpp
@@ -6,72 +6,96 @@ class ComplexNumber{
 	public:
 		double real;
 		double imag;
-		ComplexNumber(double,double);
-		ComplexNumber operator+(const ComplexNumber &);
-		ComplexNumber operator-(const ComplexNumber &);
-		ComplexNumber operator*(const ComplexNumber &);
-		ComplexNumber operator/(const ComplexNumber &);
-		bool operator==(const ComplexNumber &);
-		double abs();
-		double angle();
+
+		ComplexNumber(double x = 0,double y = 0);
+
+		ComplexNumber operator+(const ComplexNumber &) const;
+		ComplexNumber operator-(const ComplexNumber &) const;
+		ComplexNumber operator*(const ComplexNumber &) const;
+		ComplexNumber operator/(const ComplexNumber &) const;
+
+		bool operator==(const ComplexNumber &) const;
+
+		double abs() const;
+		double angle() const;
 };
 
-ComplexNumber::ComplexNumber(double x = 0,double y = 0){
-	real = x; imag = y;
-}
 
-ComplexNumber ComplexNumber::operator+(const ComplexNumber &c){
-	return ComplexNumber(real+c.real,imag+c.imag);
-}
-
-ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
-	return ComplexNumber(real-c.real,imag-c.imag);
-}
-
-//Write your code here
-
-int main(){
-	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
-	cout << a << "\n";
-	cout << b << "\n";
-	cout << c << "\n";
-	cout << a+2.5 << "\n";
-	cout << 2.5+a << "\n";
-	cout << a-1.5 << "\n";
-	cout << 1.5-a << "\n";
-	cout << b+ComplexNumber(0,2.5) << "\n";
-	cout << c-c << "\n";
-	cout << "-----------------------------------\n";
-	
-	ComplexNumber d = (a+b)/c;
-	ComplexNumber e = b/(a-c);
-	cout << d << "\n";
-	cout << e << "\n";
-	cout << c*2 << "\n";
-	cout << 0.5*c << "\n";
-	cout << 1/c << "\n";
-	cout << "-----------------------------------\n";
-	
-	cout << ComplexNumber(1,1).abs() << "\n";
-	cout << ComplexNumber(-1,1).abs() << "\n";
-	cout << ComplexNumber(1.5,2.4).abs() << "\n";
-	cout << ComplexNumber(3,4).abs() << "\n";
-	cout << ComplexNumber(69,-9).abs() << "\n";		
-	cout << "-----------------------------------\n";	
-	
-	cout << ComplexNumber(1,1).angle() << "\n";
-	cout << ComplexNumber(-1,1).angle() << "\n";
-	cout << ComplexNumber(-1,-1).angle() << "\n";
-	cout << ComplexNumber(1,-1).angle() << "\n";
-	cout << ComplexNumber(5,2).angle() << "\n";
-	cout << "-----------------------------------\n";
-	
-	cout << (ComplexNumber(1,1) == ComplexNumber(1,2)) << "\n";
-	cout << (ComplexNumber(1,1) == 1) << "\n";
-	cout << (0 == ComplexNumber()) << "\n";
+ComplexNumber::ComplexNumber(double x,double y){
+	real = x;
+	imag = y;
 }
 
 
+ComplexNumber ComplexNumber::operator+(const ComplexNumber &c) const{
+	return ComplexNumber(real + c.real, imag + c.imag);
+}
 
+ComplexNumber ComplexNumber::operator-(const ComplexNumber &c) const{
+	return ComplexNumber(real - c.real, imag - c.imag);
+}
 
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c) const{
+	return ComplexNumber(
+		real*c.real - imag*c.imag,
+		real*c.imag + imag*c.real
+	);
+}
 
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c) const{
+	double denom = c.real*c.real + c.imag*c.imag;
+	return ComplexNumber(
+		(real*c.real + imag*c.imag)/denom,
+		(imag*c.real - real*c.imag)/denom
+	);
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c) const{
+	return real == c.real && imag == c.imag;
+}
+
+double ComplexNumber::abs() const{
+	return sqrt(real*real + imag*imag);
+}
+
+double ComplexNumber::angle() const{
+	return atan2(imag,real) * 180.0 / M_PI;
+}
+
+ComplexNumber operator+(double d,const ComplexNumber &c){
+	return ComplexNumber(d) + c;
+}
+
+ComplexNumber operator-(double d,const ComplexNumber &c){
+	return ComplexNumber(d) - c;
+}
+
+ComplexNumber operator*(double d,const ComplexNumber &c){
+	return ComplexNumber(d) * c;
+}
+
+ComplexNumber operator/(double d,const ComplexNumber &c){
+	return ComplexNumber(d) / c;
+}
+
+bool operator==(double d,const ComplexNumber &c){
+	return ComplexNumber(d) == c;
+}
+
+ostream& operator<<(ostream &out,const ComplexNumber &c){
+	if(c.real == 0 && c.imag == 0){
+		out << 0;
+	}
+	else if(c.real == 0){
+		out << c.imag << "i";
+	}
+	else if(c.imag == 0){
+		out << c.real;
+	}
+	else{
+		out << c.real;
+		if(c.imag > 0) out << "+" << c.imag << "i";
+		else out << c.imag << "i";
+	}
+	return out;
+}
